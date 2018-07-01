36c2fb343c9c74ccd60a32fe846e256393ca89cf
NiVyAl <nikitavisoc@gmail.com>
2018-07-01 11:10:34 +0300

    1.15 createElement
    pre-version editor. Try to use function to create element

--- translater.js
+++ translater.js
@@ -1,4 +1,40 @@
 var buttonExit = document.querySelector('.exit-container');
+var cardContainer = document.querySelector('.card-container');
+var editorContainer = document.querySelector('.editor-container'); 
+
 buttonExit.addEventListener('click', function(){
-    alert('Я не отпущу тебя');
+    cardContainer.classList.add('none');
+    editorContainer.classList.remove('none');
 })
+
+
+var content = document.querySelector('.content');
+var footer = document.querySelector('.footer');
+
+var createCard = function(color, title, text, align) {
+    var card = document.createElement('div');
+    content.appendChild(card);
+    card.classList.add('card');
+   // card.classList.add(name);
+    card.backgroundColor = color;
+    
+    var h1 = document.createElement('h1');
+    card.appendChild(h1);
+    
+    h1.innerHTNL = title;
+    
+    var h2 = card.appendChild('h2');
+    h2.innerHTML = text;
+    
+    
+    var label = footer.appendChild('label');
+    label.for = 'btn-5';
+    label.innerHTML = '-';
+}
+
+var createElement = function(name, parents, property){
+    name = document.createElement(property);
+    parents.appendChild(name);
+}
+
+createCard('#7b4c84', 'hi', 'gfjdkd', 'center');
