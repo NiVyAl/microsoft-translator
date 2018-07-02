642872947b47f02db05dc39c5d486db4ba8fc571
NiVyAl <nikitavisoc@gmail.com>
2018-07-02 13:25:03 +0300

    1.4 input label generation in js
    have problem with elements (label) made's in js. They haven't word-spacing, but have unknown spacing. Margin is'not good decision

--- translater.js
+++ translater.js
@@ -8,13 +8,21 @@ buttonExit.addEventListener('click', function(){
 })
 
 
+//create card
+
 var content = document.querySelector('.content');
 var footer = document.querySelector('.footer');
+var cardContainer = document.querySelector('.card-container');
 
 var madeElement = function(parents, property){
     var element = document.createElement(property);
     parents.appendChild(element);
     return element;
+}
+
+var howmanyCards = function() {
+    var cards = document.querySelectorAll('.card');
+    return cards.length;
 }
 
 var createCard = function(color, title, text, align) {
@@ -29,9 +37,22 @@ var createCard = function(color, title, text, align) {
     var h2 = madeElement(card, 'h2');
     h2.innerHTML = text;
     
+    
+    var howMany = howmanyCards();
+    
+    content.style.width = howMany*100 + '%';
+    
+    var input = document.createElement('input');
+    cardContainer.insertBefore(input, cardContainer.childNodes[0]) // ставлю input перед всеми элементами в card-container
+    input.type = 'radio';
+    input.name = 'toggle';
+    input.id = 'btn-' + howMany;
+    
     var label = madeElement(footer, 'label');
-    label.htmlFor = 'btn-5';
+    label.htmlFor = 'btn-' + howMany;
     label.innerHTML = '-';
 }
 
 createCard('#7b4c84', 'Live conversations', 'Have a live, multilingual conversation in your own languag, across multiple devices.', 'center');
+
+createCard('#16ab89', 'Phrasebooks', 'Use our verified translations and pronunciation guides to help you learn important phrases while traveling', 'center');
